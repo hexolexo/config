28dc0f3dfc8e7aeb1e7457c24947d95c5e81c952
hexolexo <hexolexo132@proton.me>
2025-09-03 19:50:31 +1000

    Upgraded some scripts

--- hypr/scripts/backup.sh
+++ hypr/scripts/backup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+export GPG_TTY=$(tty)
 USER=$(whoami)
 HOST="$USER@server"
 REMOTE_PATH="/home/$USER/backups"
 LOCALHOME="/home/$USER"
 COMMAND="nohup bash /home/$USER/scripts/archive.sh > /dev/null 2>&1 & disown" # Disowns the program allowing the user to disconnect without disrupting archival
+ENCRYPTED_FILE="keys.tar.gz.gpg"
 SCRIPT="/home/$USER/.config/hypr/scripts/archive.sh"
 LOG_FILE="/home/$USER/logs/rsync.log"
+BACKUP_DIR="/tmp/secure_backup"
+
 #if ! ping server -c 1 &> /dev/null; then
 #    echo "Server Unreachable"
 #    exit 1
@@ -33,19 +37,44 @@ SOURCE_DIRS=(
     ".password-store"
     "Music"
 )
-
 for dir in "${SOURCE_DIRS[@]}"; do
-    rsync -avz --exclude='.git/' --exclude='*.pdf' --delete "$LOCALHOME/$dir" "$HOST":"$REMOTE_PATH" &
+    SRC="$LOCALHOME/$dir"
+    DEST="$HOST:$REMOTE_PATH/$dir"
+
+    if [ -d "$SRC/.git" ]; then
+        tracked_files=$(mktemp)
+        (cd "$SRC" && git ls-files > "$tracked_files")
+        rsync -avz --delete \
+            --exclude='.git/' \
+            --exclude='*.pdf' \
+            --files-from="$tracked_files" \
+            "$SRC/" "$DEST" &
+
+        rm -f "$tracked_files"
+    else
+        echo "dir is not git repo"
+        rsync -avz --delete \
+            --exclude='.git/' \
+            --exclude='*.pdf' \
+            "$SRC/" "$DEST" &
+    fi
 done
+#for dir in "${SOURCE_DIRS[@]}"; do
+    #rsync -avz --exclude='.git/' --exclude='*.pdf' --delete "$LOCALHOME/$dir" "$HOST":"$REMOTE_PATH" &
+#done
 log_message "Encrypting keys"
-BACKUP_DIR="/tmp/secure_backup_$(date +%Y%m%d)"
+
+BACKUP_DIR="/tmp/secure_backup_temp"
+SECURE_FILE="secure_keys_backup.tar.gz.gpg"
+SECURE_PATH="/tmp/$SECURE_FILE"
+
 mkdir -p "$BACKUP_DIR"
 cp -r ~/.ssh "$BACKUP_DIR/"
 cp -r ~/.gnupg "$BACKUP_DIR/"
-tar czf - -C /tmp "$(basename "$BACKUP_DIR")" | gpg -c > "$BACKUP_DIR.tar.gz.gpg"
-rsync -avz --delete "$BACKUP_DIR.tar.gz.gpg" "$HOST":"$REMOTE_PATH"
-rm -rf "$BACKUP_DIR" "$BACKUP_DIR.tar.gz.gpg"
 
+tar czf - -C /tmp "$(basename "$BACKUP_DIR")" | gpg -c > "$SECURE_PATH"
+rsync -avz "$SECURE_PATH" "$HOST:$REMOTE_PATH/$SECURE_FILE"
+rm -rf "$BACKUP_DIR" "$SECURE_PATH"
 
 wait # Wait for all to finish before continuing
 log_message "RSYNC FINISHED"
--- hypr/scripts/sshToServer.sh
+++ hypr/scripts/sshToServer.sh
@@ -16,17 +16,24 @@ command="$*"
 
 wifiname=$(nmcli connection show --active | awk 'NR>1 {print $1}' | head -n 1)
 
-if [ "$wifiname" == "$(cat ~/.secrets/home-name)" ]; then
+if [ "$wifiname" == "$(cat ~/.secrets/home-name)" ]; then # If local network
     if [ -n "$command" ]; then
         ssh $use_pty server "$command"
     else
         ssh server
     fi
-elif pgrep -x "sslocal" > /dev/null; then
+elif pgrep -x "sslocal" > /dev/null; then # If remote AND shadowsocks
     if [ -n "$command" ]; then
         proxychains4 -q ssh $use_pty server "$command"
     else
         proxychains4 -q ssh server
+    fi
+elif ip link show wg0 2>/dev/null | grep -q '<.*UP.*LOWER_UP.*>'; then
+    if [ -n "$command" ]; then
+        ssh $use_pty 10.0.0.1 -p 6000 "$command"
+    else
+        ssh 10.0.0.1 -p 6000 "$command"
+
     fi
 else
     # Fallback to home connection
